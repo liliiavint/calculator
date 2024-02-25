a200a1b0ba074044e75e3e41d33b799a12d33056
liliia_vintiuk <rudapanda2016@gmail.com>
2024-02-25 20:59:51 +0200

    paprasta be oop

--- js/functional2.js
+++ js/functional2.js
@@ -1,15 +1,25 @@
 const calculator = {
-data: ['clear', '÷','x','ce','9','8','7','-','6','5', '4', '+', '3', '2', '1', '=', '-', '.', '0', '%'],
-styledata: 'textcolor'
+    data: ['clear', '÷','x','ce','9','8','7','-','6','5', '4', '+', '3', '2', '1', '=', '.', '0', '%'],
+    styledata: 'textcolor span btnhover' 
+};
+
+const connectDOM = document.querySelector(".mygtukai");
+
+function selectTextcolor(item) {
+    
+    if (['clear', '÷', 'x', 'ce', '-', '+'].includes(item)) {
+        return 'textcolor';
+    } 
+    if (['='].includes(item)) {
+        return 'span';
+    }
+    if (['.'].includes(item)) {
+        return 'btnhover';
+    }
 }
 
-const connectDOM = document.querySelector(".mygtukai")
-
-
-
-   let HTML = '';
-    for(const item of calculator.data){
-        
-    HTML += `<button class="btn ${calculator.styledata}">${item}</button>`;
-    }
-connectDOM.innerHTML = HTML;
+let HTML = '';
+for (const item of calculator.data) {
+    HTML += `<button class="btn ${selectTextcolor(item)}">${item}</button>`;
+}
+connectDOM.innerHTML = HTML
