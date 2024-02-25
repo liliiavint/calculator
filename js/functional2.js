561c158d91c0c15d4652f193182653e12b336080
liliia_vintiuk <rudapanda2016@gmail.com>
2024-02-25 18:19:53 +0200

    update

--- js/functional2.js
+++ js/functional2.js
@@ -1,9 +1,7 @@
 const calculator = {
 data: ['clear', '÷','x','ce','9','8','7','-','6','5', '4', '+', '3', '2', '1', '=', '-', '.', '0', '%'],
-styledata: ['textcolor'+' '+'span'+' '+'btnhover']
+styledata: ['textcolor']
 }
-
-const connectDOM = document.querySelector(".mygtukai")
 
 
    let HTML = '';
